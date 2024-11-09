ecdaf11f093ac31c38503b9a8fb58679d3e73395
Igor Grabeklis <drakofm@gmail.com>
2024-11-09 10:35:26 +0300

    made class component for svg reusable deploy

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+class Progress {
+
+  constructor(
+    size = 160, 
+    strokeWidth = 11, 
+    strokeColorSubstrate = '#eff3f6', 
+    strokeColorBar = '#005bff'
+  ) {
+    this.creationTime = Date.now();
+    this.element = document.createElement('div');
+    this.element.insertAdjacentHTML('afterbegin', 
+      `
+        <svg
+        id="progress-container__bar"
+        class="progress-container__bar-class"
+        width="${size}"
+        height="${size}"
+      >
+        <circle 
+          id="progress-container__basis-circle"
+          stroke="${strokeColorSubstrate}" 
+          stroke-width="${strokeWidth}"
+          cx="${size / 2}" 
+          cy="${size / 2}"
+          r="${size / 2 - strokeWidth * 2}"
+          fill="transparent"
+        />
+        <circle 
+          id="progress-container__progress-circle"
+          stroke="${strokeColorBar}" 
+          stroke-width="${strokeWidth}"
+          cx="${size / 2}" 
+          cy="${size / 2}"
+          r="${size / 2 - strokeWidth * 2}"  
+          fill="transparent"
+        />
+      </svg>
+      `
+    );
+  }
+
+  insertAfter(node) {
+    node.after(this.element);
+  }
+
+  makeColor() {
+    document.querySelector('#progress-container__progress-circle').style.stroke = 'red';
+    setTimeout(() => {document.querySelector('#progress-container__progress-circle').style.stroke = '#005bff';}, 1000);
+  }
+
+}
+
+const a = new Progress();
+a.insertAfter(document.querySelector('#progress-container__headline'));
+
 const progressBlock = document.querySelector('#progress-container__bar');
 const progressBarLine = document.querySelector('#progress-container__progress-circle');
 const progressBarLineRadius = progressBarLine.r.baseVal.value;
