d86c8c26c401d42540fdbe9961514efea4c72b6e
Igor Grabeklis <drakofm@gmail.com>
2024-11-10 18:04:48 +0300

    > removed all hardcoded html, now it completly builds dynamically from 'Progress' class > made unique id-s for all created elements, bounded 'Progress'-instances-shared styles to CSS classses > moved event listeners initialization to class method 'setEventListeners'

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-class ProgressBar {
+class Progress {
 
   constructor(
+    name = 'Progress',
     size = 160, 
     strokeWidth = 11, 
     strokeColorSubstrate = '#eff3f6', 
@@ -8,39 +9,97 @@ class ProgressBar {
     transitionSpeedSeconds = 0.1,
     animationPeriodSeconds = 1,
   ) {
-    this.creationTime = Date.now();
+    this.id = Math.floor(Math.random() * 1e9);
     this.rootElement = document.createElement('div');
+    this.rootElement.id = `progress-container-${this.id}`;
+    this.rootElement.className = `progress-container`;
     this.rootElement.insertAdjacentHTML('afterbegin', 
       `
+        <p 
+          id="progress-container__headline-${this.id}" 
+          class="progress-container__headline"
+        >
+         ${name}
+        </p>
+
         <svg
-        id="progress-container__bar-${this.creationTime}"
-        width="${size}"
-        height="${size}"
-      >
-        <circle 
-          id="progress-container__basis-circle-${this.creationTime}"
-          stroke="${strokeColorSubstrate}" 
-          stroke-width="${strokeWidth}"
-          cx="${size / 2}" 
-          cy="${size / 2}"
-          r="${size / 2 - strokeWidth * 2}"
-          fill="transparent"
-        />
-        <circle 
-          id="progress-container__progress-circle-${this.creationTime}"
-          stroke="${strokeColorBar}" 
-          stroke-width="${strokeWidth}"
-          cx="${size / 2}" 
-          cy="${size / 2}"
-          r="${size / 2 - strokeWidth * 2}"  
-          fill="transparent"
-        />
-      </svg>
+          id="progress-container__bar-${this.id}"
+          class="progress-container__bar"
+          width="${size}"
+          height="${size}"
+        >
+          <circle 
+            id="progress-container__basis-circle-${this.id}"
+            class="progress-container__basis-circle"
+            stroke="${strokeColorSubstrate}" 
+            stroke-width="${strokeWidth}"
+            cx="${size / 2}" 
+            cy="${size / 2}"
+            r="${size / 2 - strokeWidth * 2}"
+            fill="transparent"
+          />
+          <circle 
+            id="progress-container__progress-circle-${this.id}"
+            class="progress-container__progress-circle"
+            stroke="${strokeColorBar}" 
+            stroke-width="${strokeWidth}"
+            cx="${size / 2}" 
+            cy="${size / 2}"
+            r="${size / 2 - strokeWidth * 2}"  
+            fill="transparent"
+          />
+        </svg>
+
+        <section id="progress-container__controls-${this.id}" class="progress-container__controls">
+
+          <div class="progress-container__control-container">
+            <input 
+              id="progress-container__value-input-${this.id}"
+              class="progress-container__value-input" 
+              type="text" 
+              value="0"
+            > 
+            <label for="progress-container__value-input-${this.id}">Value</label>
+          </div>
+
+
+          <div class="progress-container__control-container">
+
+            <label for="progress-container__animate-toggle-${this.id}" class="progress-container__label">
+              <input 
+              id="progress-container__animate-toggle-${this.id}" 
+              class="progress-container__input" 
+              type="checkbox"
+              >
+              <span class="progress-container__slider"></span>
+            </label>
+
+            <label for="progress-container__animate-toggle-${this.id}">Animate</label>
+          </div>
+
+
+          <div class="progress-container__control-container">
+
+            <label for="progress-container__hide-toggle-${this.id}" class="progress-container__label">
+              <input id="progress-container__hide-toggle-${this.id}" class="progress-container__input" type="checkbox">
+              <span class="progress-container__slider"></span>
+            </label> 
+
+            <label for="progress-container__hide-toggle-${this.id}" class="progress-container__input">Hide</label>
+
+          </div>
+
+        </section>
       `
     );
-    this.rootElement.style.transition = `${transitionSpeedSeconds}s`;
-    this.basisCircle = this.rootElement.firstElementChild.children[0];
-    this.progressCircle = this.rootElement.firstElementChild.children[1];
+    this.barCircles = this.rootElement.children[1];
+    this.basisCircle = this.barCircles.children[0];
+    this.progressCircle = this.barCircles.children[1];
+    this.progressPercentInput = this.rootElement.children[2].children[0].children[0];
+    this.togglerAnimation = this.rootElement.children[2].children[1].children[0];
+    this.togglerHide = this.rootElement.children[2].children[2].children[0];
+
+    this.barCircles.style.transition = `${transitionSpeedSeconds}s`;
     this.progressCircle.style.transformOrigin = 'center';
     this.progressCircle.style.transform = 'rotate(-90deg)';
     this.progressCircle.style.transition = `stroke-dashoffset ${transitionSpeedSeconds}s`;
@@ -74,10 +133,6 @@ class ProgressBar {
     this.progressCircleAnimation = new Animation(this.progressCircleKeyframeEffect);
   }
 
-  insertAfter(node) {
-    node.after(this.rootElement);
-  }
-
   setProgressPercent(percent) {
     percent > 100 ? percent = 100 : percent < 0 ? percent = 0: '';
     this.progressCircle.style.strokeDashoffset = this.progressCircleCircumference - percent / 100 * this.progressCircleCircumference;
@@ -93,33 +148,29 @@ class ProgressBar {
   
   switchHiding(isChecked) {
     if (isChecked) {
-      this.rootElement.style.opacity = 0;
+      this.barCircles.style.opacity = 0;
     } else {
-      this.rootElement.style.opacity = 1;
+      this.barCircles.style.opacity = 1;
     }
   }
 
-  // TODO: анимации нужно перенести из CSS в JS
-
+  setEventListeners() {
+    this.progressPercentInput.addEventListener('change', () => {
+      this.setProgressPercent(+this.progressPercentInput.value);
+    });
+  
+    this.togglerAnimation.addEventListener('change', (event) => {
+      this.switchLoadingAnimation(event.target.checked);
+    });
+  
+    this.togglerHide.addEventListener('change', (event) => {
+      this.switchHiding(event.target.checked);
+    });
+  }
 }
 
-const firstProgressBar = new ProgressBar();
-firstProgressBar.insertAfter(document.querySelector('#progress-container__headline'));
-
-const progressPercentInput = document.querySelector('#progress-container__value-input');
-const togglerAnimation = document.querySelector('#progress-container__animate-toggle');
-const togglerHide = document.querySelector('#progress-container__hide-toggle');
 
 
-
-progressPercentInput.addEventListener('change', () => {
-  firstProgressBar.setProgressPercent(+progressPercentInput.value);
-});
-
-togglerAnimation.addEventListener('change', (event) => {
-  firstProgressBar.switchLoadingAnimation(event.target.checked);
-});
-
-togglerHide.addEventListener('change', (event) => {
-  firstProgressBar.switchHiding(event.target.checked);
-});
+const firstProgress = new Progress();
+document.body.insertAdjacentElement('afterbegin', firstProgress.rootElement);
+firstProgress.setEventListeners();
